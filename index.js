1303075d9e72dcbbf9e8bb2946fb0dde0bdf5651
Igor Parchinski <parczynski@gmail.com>
2023-03-02 20:23:35 +0400

    generate project

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 #!/usr/bin/env node
 
-const fs = require( 'fs' )
+
+// Usage: npx new-ts-app [name]
+  
+const { mkdir, cp, writeFile } = require( 'fs' ).promises
+const { write } = require('fs');
 const path = require( 'path' )
-
-// Usage: npx create-ts-app [name]
-
-const projectName = process.argv[2];
+const { ask, stopReadline } = require('./utils');
 
 
-const currentDir = process.cwd();
-const projectDir = path.resolve(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+
+
+;( async () => {
+
+	try{
+
+		const source = path.resolve( process.cwd(), 'template' );
+		const package = require( path.join( source, 'package.json' ) )
+
+		package.name = typeof process.argv[2] === 'undefined'
+			? await ask( 'Project name: ' )
+			: process.argv[2]
+
+		
+		const target = path.resolve( process.cwd(), package.name );
+
+
+		// Create project directory
+		await mkdir( target, { recursive: true } )
+
+
+		// Copy template to the target
+		await cp( source, target, { recursive: true } );
+
+
+		// Write project info
+		await writeFile( path.join( target, 'package.json' ), JSON.stringify( package, null, 2 ) )
+		
+	} catch( err ) {
+		console.error( err )
+	} finally {
+		stopReadline()
+	}
+	
+
+})()
